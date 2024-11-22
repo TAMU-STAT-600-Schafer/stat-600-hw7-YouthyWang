3d24449da9c5662bdb5e1dca26ce2672f5dd6d31
Youthy WANG <youthywang@tamu.edu>
2024-11-22 09:32:47 -0600

    Some simple tests of functions in function_NN, 2024/11/12.

--- Test.R
+++ Test.R
@@ -1,3 +1,10 @@
 source("FunctionsNN.R")
 
-out1 <- initialize_bw(16, 20, 10, 1e-3)
+out1 <- initialize_bw(16, 20, 10, 1e-3);
+
+y <- c(1,2,3,4,5,1,2,3,4,5);
+scores <- matrix(rnorm(10*5), 10, 5);
+K <- 5;
+
+out2 <- loss_grad_scores(y, scores, K)
+
